f97984759557537b6450766b5c7c5b867a83d54c
IsraaMohamed <122988145+IsraaXx@users.noreply.github.com>
2024-04-12 16:22:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-   const loginForm = document.getElementById("logform");
-   const signupForm = document.getElementById("signform");
+  const signupForm = document.getElementById("signform");
 
-    if(loginForm){
-    loginForm.addEventListener("submit", function(event)  {
-        event.preventDefault(); 
-       window.location.href="Home-page.html";
+       signupForm.addEventListener("submit", function(event) {
+         event.preventDefault(); 
+         window.location.href= "Login.html";
+       });
+   
 
-    });
-}
-
-        signupForm.addEventListener("submit", function(event) {
-          event.preventDefault(); 
-          window.location.href= "Login.html";
-        });
-    
-
-    });
+   });
